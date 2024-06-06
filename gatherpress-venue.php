3938fb9db38b5110ba1cbb16eaf43460d4fbb0f7
Carsten Bach <mail@carsten-bach.de>
2024-06-06 15:21:43 +0200

    Remove Declaration of 'Requires Plugins:' until GatherPress is in the wp.org repo

--- gatherpress-venue.php
+++ gatherpress-venue.php
@@ -5,7 +5,6 @@
  * Version:           0.1.0-alpha
  * Requires at least: 6.5.3
  * Requires PHP:      8.1
- * Requires Plugins:  gatherpress
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
